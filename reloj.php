2f2d6d0ceb1046d9c8fe04b5b0684e1a042f401c
pachangueros <46282577+pachangueros@users.noreply.github.com>
2020-07-05 08:46:24 +0200

    Add files via upload

---
+++ reloj.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Safe Backpack ID</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+    <body>
+       AAMMDDHHMMSS<br>
+       200731235530
+    </body>
+</html>
